d5e89001019c0db759e982131d0ea24c41ce1d9d
Mnari <mnpinvest@yahoo.com>
2026-06-11 18:36:41 +0200

    Add admin interface theme

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,6 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # ============================================================
 
 INSTALLED_APPS = [
+    # Thème admin (AJOUT)
+    "colorfield",
+    "admin_interface",
+
     # Django
     "django.contrib.admin",
     "django.contrib.auth",
